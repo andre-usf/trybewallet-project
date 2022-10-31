5cfb1f43eb8e88af9503039ef4521c02055297eb
andre-usf <andrefretta@gmail.com>
2022-10-31 17:23:16 -0300

    Inicia configuração do Redux.

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE_USER = {
+  user: {
+    email: '', // string que armazena o email da pessoa usuária
+  },
+};
+
+function walletReducer(state = INITIAL_STATE_USER, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const INITIAL_STATE_WALLET = {
+  wallet: {
+    currencies: [], // array de string
+    expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
+    editor: false, // valor booleano que indica de uma despesa está sendo editada
+    idToEdit: 0, // valor numérico que armazena o id da despesa que esta sendo editada
+  },
+};
+
+function loginReducer(state = INITIAL_STATE_WALLET, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default loginReducer;
